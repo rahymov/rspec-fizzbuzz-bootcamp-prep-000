6a6811a345f4da98035c1c30ec08fb63bce3106f
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-30 15:39:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@ def fizzbuzz(number)
   if number % 3 == 0
    "Fizz"
   elsif number % 5 == 0
-   "Buzz"
+   return "Buzz"
   elsif number % 3 == 0 && number % 5 == 0
     return "FizzBuzz" 
   else
